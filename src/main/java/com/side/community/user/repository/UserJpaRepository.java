70b7690c4d7045de8d0cdf7f58aaa32ae91aa7b4
legoChoi <chlx159@gmail.com>
2025-08-27 18:22:51 +0900

    :sparkles: feat: AUTH 로그인 API 추가

--- src/main/java/com/side/community/user/repository/UserJpaRepository.java
+++ src/main/java/com/side/community/user/repository/UserJpaRepository.java
@@ -3,8 +3,14 @@ package com.side.community.user.repository;
 import com.side.community.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<User, Long> {
     boolean existsByAccountId(String accountId);
 
     boolean existsByNickname(String nickname);
+
+    Optional<User> findByAccountId(String accountId);
 }
