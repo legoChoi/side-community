55f3c7509f4e378924014726eaf22554a5f83fbf
legoChoi <chlx159@gmail.com>
2025-08-28 11:33:15 +0900

    :sparkles: feat: JPA Auditing 엔티티 추가 및 적용

--- src/main/java/com/side/community/common/entity/BaseEntity.java
+++ src/main/java/com/side/community/common/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package com.side.community.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,8 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+
+@Getter
 public abstract class BaseEntity {
 
     @CreatedDate
